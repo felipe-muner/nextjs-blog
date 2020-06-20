dc5c7c9e02deb59b5da25de8ff42675333082cd3
felipemuner <felipe.muner@gmail.com>
2020-06-20 10:50:11 +0700

    import a external data file and render it to the user using markdown file

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import Alert from "../components/Alert";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
+  console.log("pritnareio");
+  console.log(allPostsData);
   return (
     <Layout home>
       <Head>
@@ -14,9 +17,32 @@ export default function Home() {
         <Alert type="error">ola</Alert>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>
+          .)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
+}
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
